242398612f6b2d36563e1f6fc67bfe36d148dd1a
RomanKhrabatyn <romankhr1974@gmail.com>
2023-05-29 17:14:38 +0300

    caypress first commit

---
+++ cypress/integration/login/login.ts
@@ -0,0 +1,17 @@
+import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
+// import cypress = require("cypress");
+import LoginPage from "../../pages/loginPage";
+
+Given('User opens login page', () => {
+
+  cy.visit('https://qas-eastus-hrvyst-app.azurewebsites.net/login')
+  
+});
+
+When('Click login button on login page', () =>{
+  LoginPage.clickLoginButton();
+})
+
+Then('Insert username {string} and password {string} on on login page', (username, password)=>{
+  LoginPage.login(username, password);
+})
